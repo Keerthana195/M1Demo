2319ccee1a646cc9d95eea1a30f2f0a417672cb1
Keerthana <Keerthana@keerthana>
2026-04-13 11:22:21 +0530

    master to ATE

--- src/main/java/demo/Practice2.java
+++ src/main/java/demo/Practice2.java
@@ -5,6 +5,7 @@ public class Practice2 {
 	public static void main(String[] args) {
 		System.out.println("changes in git");
 		System.out.println("In branch");
+		System.out.println("change master to ATE");
 	}
 
 }
